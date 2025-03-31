557c0975c88bf22a0187fae222be644404693b54
Dsk Projects <dikshant07kamble@gmail.com>
2025-04-01 02:01:18 +0530

    value

--- src/main/java/com/nt/App.java
+++ src/main/java/com/nt/App.java
@@ -4,7 +4,9 @@ package com.nt;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+	{		
+		int j = 20;
         System.out.println("Hello World!");
     }
 }
