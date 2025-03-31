224882a2ccd57d651afc0912d58e8a9aa583e2fd
Dikshant Kamble <140292841+dsk1891@users.noreply.github.com>
2025-04-01 01:39:18 +0530

    Update App.java

--- src/main/java/com/nt/App.java
+++ src/main/java/com/nt/App.java
@@ -4,7 +4,9 @@ package com.nt;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        string s = "hii";
         System.out.println("Hello World!");
     }
 }
